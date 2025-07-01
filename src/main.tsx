494fd9df9001bf51006040fc71ca9a8559203026
Evelyn Martins <evelynlarissa220@gmail.com>
2025-06-30 23:35:39 -0300

    remove clerk key

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,11 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { ClerkProvider } from "@clerk/clerk-react";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
-      </ClerkProvider>
     </Provider>
   </StrictMode>
 );
